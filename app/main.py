6cf1bf203226a2441fdfdbd7dca4436b1959e9e2
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-01-26 21:26:24 +0200

    Add robust validation system with Validator descriptors and implement BurgerRecipe class

--- app/main.py
+++ app/main.py
@@ -1,14 +1,89 @@
-class Validator:
-    pass
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
 
 
-class Number:
-    pass
+class MyErrors(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
 
 
-class OneOf:
-    pass
+class Validator(ABC):
+
+    def __set_name__(self, owner: Validator, name: str) -> None:
+        self.protected_name = f"_{name}"
+
+    def __get__(self,
+                instance: "Validator",
+                owner: str
+                ) -> str | int:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self,
+                instance: "Validator",
+                value: int | str
+                ) -> None:
+        self.validate(value)
+        setattr(instance, self.protected_name, value)
+
+    @abstractmethod
+    def validate(self, value: int | str) -> None:
+        pass
+
+
+class Number(Validator):
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value: int) -> int:
+        if not isinstance(value, int | str):
+            raise TypeError("Quantity should be integer.")
+        if value not in range(self.min_value, self.max_value + 1):
+            raise ValueError(f"Quantity should not be less than "
+                             f"{self.min_value} and greater than "
+                             f"{self.max_value}.")
+        return value
+
+
+class OneOf(Validator):
+    def __init__(self, options: tuple) -> None:
+        self.options = options
+
+    def validate(self, value: str) -> str:
+        if value not in self.options:
+            raise ValueError(f"Expected {value} "
+                             f"to be one of {self.options}.")
+        return value
 
 
 class BurgerRecipe:
+    buns = Number(2, 3)
+    cheese = Number(0, 2)
+    tomatoes = Number(0, 3)
+    cutlets = Number(1, 3)
+    eggs = Number(0, 2)
+    sauce = OneOf(("ketchup", "mayo", "burger"))
+
+    def __init__(self,
+                 buns: int,
+                 cheese: int,
+                 tomatoes: int,
+                 cutlets: int,
+                 eggs: int,
+                 sauce: str) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
+
+    def __repr__(self) -> str:
+        values = ", ".join(f"{key!r}={value!r}"
+                           for key, value in self.__dict__.items())
+        return f"{self.__class__.__name__} {values}"
+
+
+if __name__ == "__main__":
     pass
